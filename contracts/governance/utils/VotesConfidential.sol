b32b147b6c65fbcaf996e8d3aa9dfcea10224f91
Arr00 <13561405+arr00@users.noreply.github.com>
2025-05-29 18:53:35 -0400

    reorder functions and add docs

--- contracts/governance/utils/VotesConfidential.sol
+++ contracts/governance/utils/VotesConfidential.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { IERC6372 } from "@openzeppelin/contracts/interfaces/IERC6372.sol";
 import { TFHE, einput, euint64 } from "fhevm/lib/TFHE.sol";
-import { Time } from "@openzeppelin/contracts/utils/types/Time.sol";
+import { IERC6372 } from "@openzeppelin/contracts/interfaces/IERC6372.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import { Time } from "@openzeppelin/contracts/utils/types/Time.sol";
 
 import { CheckpointConfidential } from "../../utils/structs/CheckpointConfidential.sol";
 
@@ -21,40 +21,30 @@ abstract contract VotesConfidential is IERC6372 {
 
     CheckpointConfidential.TraceEuint64 private _totalCheckpoints;
 
+    /// @dev Emitted when a token transfer or delegate change results in changes to a delegate's number of voting units.
     event DelegateVotesChanged(address indexed delegate, euint64 previousVotes, euint64 newVotes);
 
-    /**
-     * @dev Emitted when an account changes their delegate.
-     */
+    /// @dev Emitted when an account changes their delegate.
     event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
 
-    /**
-     * @dev The clock was incorrectly modified.
-     */
+    /// @dev The clock was incorrectly modified.
     error ERC6372InconsistentClock();
 
-    /**
-     * @dev Lookup to future votes is not available.
-     */
+    /// @dev Lookup to future votes is not available.
     error ERC5805FutureLookup(uint256 timepoint, uint48 clock);
 
-    function _validateTimepoint(uint256 timepoint) internal view returns (uint48) {
-        uint48 currentTimepoint = clock();
-        if (timepoint >= currentTimepoint) revert ERC5805FutureLookup(timepoint, currentTimepoint);
-        return SafeCast.toUint48(timepoint);
-    }
-
     /**
-     * @dev Returns the current amount of votes that `account` has.
+     * @dev Clock used for flagging checkpoints. Can be overridden to implement timestamp based
+     * checkpoints (and voting), in which case {CLOCK_MODE} should be overridden as well to match.
      */
-    function getVotes(address account) public view virtual returns (euint64) {
-        return _delegateCheckpoints[account].latest();
-    }
-
     function clock() public view virtual returns (uint48) {
         return Time.blockNumber();
     }
 
+    /**
+     * @dev Machine-readable description of the clock as specified in ERC-6372.
+     */
+    // solhint-disable-next-line func-name-mixedcase
     function CLOCK_MODE() public view virtual returns (string memory) {
         // Check that the clock was not modified
         if (clock() != Time.blockNumber()) {
@@ -63,26 +53,59 @@ abstract contract VotesConfidential is IERC6372 {
         return "mode=blocknumber&from=default";
     }
 
+    /// @dev Returns the current amount of votes that `account` has.
+    function getVotes(address account) public view virtual returns (euint64) {
+        return _delegateCheckpoints[account].latest();
+    }
+
+    /**
+     * @dev Returns the amount of votes that `account` had at a specific moment in the past. If the `clock()` is
+     * configured to use block numbers, this will return the value at the end of the corresponding block.
+     *
+     * Requirements:
+     *
+     * - `timepoint` must be in the past. If operating using block numbers, the block must be already mined.
+     */
     function getPastVotes(address account, uint256 timepoint) public view virtual returns (euint64) {
         return _delegateCheckpoints[account].upperLookupRecent(_validateTimepoint(timepoint));
     }
 
+    /**
+     * @dev Returns the total supply of votes available at a specific moment in the past. If the `clock()` is
+     * configured to use block numbers, this will return the value at the end of the corresponding block.
+     *
+     * NOTE: This value is the sum of all available votes, which is not necessarily the sum of all delegated votes.
+     * Votes that have not been delegated are still part of total supply, even though they would not participate in a
+     * vote.
+     *
+     * Requirements:
+     *
+     * - `timepoint` must be in the past. If operating using block numbers, the block must be already mined.
+     */
     function getPastTotalSupply(uint256 timepoint) public view virtual returns (euint64) {
         return _totalCheckpoints.upperLookupRecent(_validateTimepoint(timepoint));
     }
 
+    /// @dev Returns the current total supply of votes.
     function getCurrentTotalSupply() public view virtual returns (euint64) {
         return _totalCheckpoints.latest();
     }
 
+    /// @dev Returns the delegate that `account` has chosen.
     function delegates(address account) public view virtual returns (address) {
         return _delegatee[account];
     }
 
+    /// @dev Delegates votes from the sender to `delegatee`.
     function delegate(address delegatee) public virtual {
         _delegate(msg.sender, delegatee);
     }
 
+    /**
+     * @dev Delegate all of `account`'s voting units to `delegatee`.
+     *
+     * Emits events {IVotes-DelegateChanged} and {IVotes-DelegateVotesChanged}.
+     */
     function _delegate(address account, address delegatee) internal virtual {
         address oldDelegate = delegates(account);
         _delegatee[account] = delegatee;
@@ -91,6 +114,10 @@ abstract contract VotesConfidential is IERC6372 {
         _moveDelegateVotes(oldDelegate, delegatee, _getVotingUnits(account));
     }
 
+    /**
+     * @dev Transfers, mints, or burns voting units. To register a mint, `from` should be zero. To register a burn, `to`
+     * should be zero. Total supply of voting units will be adjusted with mints and burns.
+     */
     function _transferVotingUnits(address from, address to, euint64 amount) internal virtual {
         if (from == address(0)) {
             euint64 newValue = _totalCheckpoints.latest().add(amount);
@@ -107,6 +134,9 @@ abstract contract VotesConfidential is IERC6372 {
         _moveDelegateVotes(delegates(from), delegates(to), amount);
     }
 
+    /**
+     * @dev Moves delegated votes from one delegate to another.
+     */
     function _moveDelegateVotes(address from, address to, euint64 amount) internal virtual {
         CheckpointConfidential.TraceEuint64 storage store;
         if (from != to && euint64.unwrap(amount) != 0) {
@@ -129,13 +159,20 @@ abstract contract VotesConfidential is IERC6372 {
         }
     }
 
-    function _push(CheckpointConfidential.TraceEuint64 storage store, euint64 value) private returns (euint64) {
-        (euint64 oldValue, ) = store.push(clock(), value);
-        return oldValue;
+    /// @dev Validate that a timepoint is in the past, and return it as a uint48.
+    function _validateTimepoint(uint256 timepoint) internal view returns (uint48) {
+        uint48 currentTimepoint = clock();
+        if (timepoint >= currentTimepoint) revert ERC5805FutureLookup(timepoint, currentTimepoint);
+        return SafeCast.toUint48(timepoint);
     }
 
     /**
      * @dev Must return the voting units held by an account.
      */
     function _getVotingUnits(address) internal view virtual returns (euint64);
+
+    function _push(CheckpointConfidential.TraceEuint64 storage store, euint64 value) private returns (euint64) {
+        (euint64 oldValue, ) = store.push(clock(), value);
+        return oldValue;
+    }
 }
