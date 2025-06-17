f1effb4dcb636336b92e0959d5e68fef0a00eaad
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-17 10:37:04 +0200

    Update SwapConfidentialFungibleTokenToConfidentialFungibleToken.sol

--- contracts/mocks/docs/SwapConfidentialFungibleTokenToConfidentialFungibleToken.sol
+++ contracts/mocks/docs/SwapConfidentialFungibleTokenToConfidentialFungibleToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {einput, euint64, TFHE} from "fhevm/lib/TFHE.sol";
+import {TFHE, einput, euint64} from "fhevm/lib/TFHE.sol";
 import {IConfidentialFungibleToken} from "../../interfaces/IConfidentialFungibleToken.sol";
 
 contract SwapConfidentialFungibleTokenToConfidentialFungibleToken {
