16cc058b54592e62526d39c8d6fc00e222067363
Arr00 <13561405+arr00@users.noreply.github.com>
2025-06-18 17:20:45 -0400

    add to docs

--- scripts/generate/templates/CheckpointsConfidential.js
+++ scripts/generate/templates/CheckpointsConfidential.js
@@ -12,7 +12,7 @@ import {${OPTS.map(opt => opt.valueTypeName).join(', ')}} from "fhevm/lib/TFHE.s
  * @dev This library defines the \`Trace*\` struct, for checkpointing values as they change at different points in
  * time, and later looking up past values by block number.
  *
- * To create a history of checkpoints, define a variable type \`Checkpoints.Trace*\` in your contract, and store a new
+ * To create a history of checkpoints, define a variable type \`CheckpointsConfidential.Trace*\` in your contract, and store a new
  * checkpoint for the current transaction block using the {push} function.
  */
 `;
