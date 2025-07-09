dbb18afad8296366d9f6946ecb1593da76bcd2d9
Arr00 <13561405+arr00@users.noreply.github.com>
2025-07-09 12:42:03 -0600

    update pragmas

--- contracts/finance/VestingWalletCliffConfidential.sol
+++ contracts/finance/VestingWalletCliffConfidential.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {euint64} from "@fhevm/solidity/lib/FHE.sol";
 import {VestingWalletConfidential} from "./VestingWalletConfidential.sol";
