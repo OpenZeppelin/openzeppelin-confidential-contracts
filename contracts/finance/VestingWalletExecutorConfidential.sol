3bff67ff15b8a08a25443e2a879bba1fa9ddab81
Arr00 <13561405+arr00@users.noreply.github.com>
2025-07-11 10:10:55 -0600

    Update `ERC7821WithExecutor`

--- contracts/finance/VestingWalletExecutorConfidential.sol
+++ contracts/finance/VestingWalletExecutorConfidential.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {ERC7821} from "@openzeppelin/contracts/account/extensions/draft-ERC7821.sol";
 import {VestingWalletConfidential} from "./VestingWalletConfidential.sol";
 
@@ -8,32 +9,31 @@ import {VestingWalletConfidential} from "./VestingWalletConfidential.sol";
  * @dev Extension of {VestingWalletConfidential} that adds an {executor} role able to perform arbitrary
  * calls on behalf of the vesting wallet (e.g. to vote, stake, or perform other management operations).
  */
-//TODO: Rename file/name to WithExecutor
-abstract contract VestingWalletExecutorConfidential is VestingWalletConfidential, ERC7821 {
-    /// @custom:storage-location erc7201:openzeppelin.storage.VestingWalletExecutorConfidential
-    struct VestingWalletExecutorStorage {
+abstract contract ERC7821WithExecutor is Initializable, VestingWalletConfidential, ERC7821 {
+    /// @custom:storage-location erc7201:openzeppelin.storage.ERC7821WithExecutor
+    struct ERC7821WithExecutorStorage {
         address _executor;
     }
 
-    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.VestingWalletExecutorConfidential")) - 1)) & ~bytes32(uint256(0xff))
+    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ERC7821WithExecutor")) - 1)) & ~bytes32(uint256(0xff))
     // solhint-disable-next-line const-name-snakecase
-    bytes32 private constant VestingWalletExecutorStorageLocation =
-        0x165c39f99e134d4ac22afe0db4de9fbb73791548e71f117f46b120e313690700;
+    bytes32 private constant ERC7821WithExecutorStorageLocation =
+        0x246106ffca67a7d3806ba14f6748826b9c39c9fa594b14f83fe454e8e9d0dc00;
 
-    function _getVestingWalletExecutorStorage() private pure returns (VestingWalletExecutorStorage storage $) {
+    function _getERC7821WithExecutorStorage() private pure returns (ERC7821WithExecutorStorage storage $) {
         assembly {
-            $.slot := VestingWalletExecutorStorageLocation
+            $.slot := ERC7821WithExecutorStorageLocation
         }
     }
 
     // solhint-disable-next-line func-name-mixedcase
-    function __VestingWalletExecutorConfidential_init(address executor_) internal {
-        _getVestingWalletExecutorStorage()._executor = executor_;
+    function __ERC7821WithExecutor_init(address executor_) internal onlyInitializing {
+        _getERC7821WithExecutorStorage()._executor = executor_;
     }
 
-    /// @dev Trusted address that is able to execute arbitrary calls from the vesting wallet via {call}.
+    /// @dev Trusted address that is able to execute arbitrary calls from the vesting wallet via `ERC7821`.
     function executor() public view virtual returns (address) {
-        return _getVestingWalletExecutorStorage()._executor;
+        return _getERC7821WithExecutorStorage()._executor;
     }
 
     function _erc7821AuthorizedExecutor(
