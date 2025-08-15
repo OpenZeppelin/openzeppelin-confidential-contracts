f19b3b5e0780e96947b0ffc65d60ae0b12cc6931
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-15 14:31:26 -0400

    up

--- contracts/mocks/token/ERC7984ERC20WrapperMock.sol
+++ contracts/mocks/token/ERC7984ERC20WrapperMock.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.27;
 
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {ConfidentialFungibleTokenERC20Wrapper, ConfidentialFungibleToken} from "../../token/extensions/ConfidentialFungibleTokenERC20Wrapper.sol";
+import {ERC7984ERC20Wrapper, ERC7984} from "../../token/ERC7984/extensions/ERC7984ERC20Wrapper.sol";
 
-contract ConfidentialFungibleTokenERC20WrapperMock is ConfidentialFungibleTokenERC20Wrapper, SepoliaConfig {
+contract ERC7984ERC20WrapperMock is ERC7984ERC20Wrapper, SepoliaConfig {
     constructor(
         IERC20 token,
         string memory name,
         string memory symbol,
         string memory uri
-    ) ConfidentialFungibleTokenERC20Wrapper(token) ConfidentialFungibleToken(name, symbol, uri) {}
+    ) ERC7984ERC20Wrapper(token) ERC7984(name, symbol, uri) {}
 }
