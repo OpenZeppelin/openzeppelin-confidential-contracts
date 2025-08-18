457e61d23b0d451effdc33750a50d6c601088ded
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-18 10:37:41 -0400

    reuse token mock

--- contracts/mocks/token/ConfidentialFungibleTokenCustodianAccessMock.sol
+++ contracts/mocks/token/ConfidentialFungibleTokenCustodianAccessMock.sol
@@ -5,24 +5,28 @@ pragma solidity ^0.8.27;
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
 import {ConfidentialFungibleTokenCustodianAccess, ConfidentialFungibleToken} from "../../token/extensions/ConfidentialFungibleTokenCustodianAccess.sol";
+import {ConfidentialFungibleTokenMock} from "./ConfidentialFungibleTokenMock.sol";
 
-// solhint-disable func-name-mixedcase
-contract ConfidentialFungibleTokenCustodianAccessMock is ConfidentialFungibleTokenCustodianAccess, SepoliaConfig {
-    address private immutable _OWNER;
-
+contract ConfidentialFungibleTokenCustodianAccessMock is
+    ConfidentialFungibleTokenCustodianAccess,
+    ConfidentialFungibleTokenMock
+{
     constructor(
         string memory name_,
         string memory symbol_,
         string memory tokenURI_
-    ) ConfidentialFungibleToken(name_, symbol_, tokenURI_) {
-        _OWNER = msg.sender;
-    }
+    ) ConfidentialFungibleTokenMock(name_, symbol_, tokenURI_) {}
 
-    function $_mint(
+    function _update(
+        address from,
         address to,
-        externalEuint64 encryptedAmount,
-        bytes calldata inputProof
-    ) public returns (euint64 transferred) {
-        return _mint(to, FHE.fromExternal(encryptedAmount, inputProof));
+        euint64 amount
+    )
+        internal
+        virtual
+        override(ConfidentialFungibleTokenCustodianAccess, ConfidentialFungibleTokenMock)
+        returns (euint64)
+    {
+        return super._update(from, to, amount);
     }
 }
