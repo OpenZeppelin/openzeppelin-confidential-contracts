a8821449391a6e578bb33ae77f18979f1d9926ff
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-19 10:32:17 -0400

    Remove account from `_validateHandleAllowance`

--- contracts/utils/HandleAccessManager.sol
+++ contracts/utils/HandleAccessManager.sol
@@ -12,7 +12,7 @@ abstract contract HandleAccessManager {
      * {_validateHandleAllowance} function.
      */
     function getHandleAllowance(bytes32 handle, address account, bool persistent) public virtual {
-        _validateHandleAllowance(handle, account);
+        _validateHandleAllowance(handle);
         if (persistent) {
             Impl.allow(handle, account);
         } else {
@@ -24,5 +24,5 @@ abstract contract HandleAccessManager {
      * @dev Unimplemented function that must revert if the message sender is not allowed to call
      * {getHandleAllowance} for the given handle.
      */
-    function _validateHandleAllowance(bytes32 handle, address account) internal view virtual;
+    function _validateHandleAllowance(bytes32 handle) internal view virtual;
 }
