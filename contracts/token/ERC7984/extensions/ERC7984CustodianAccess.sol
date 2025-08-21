5dd3c8660a54869d8acb5ffa3d82be730e0184ac
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-21 11:19:30 -0400

    remove unused imports

--- contracts/token/ERC7984/extensions/ERC7984CustodianAccess.sol
+++ contracts/token/ERC7984/extensions/ERC7984CustodianAccess.sol
@@ -2,12 +2,7 @@
 
 pragma solidity ^0.8.27;
 
-import {FHE, externalEuint64, euint64} from "@fhevm/solidity/lib/FHE.sol";
-import {IERC1363Receiver} from "@openzeppelin/contracts/interfaces/IERC1363Receiver.sol";
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
 import {ERC7984} from "../ERC7984.sol";
 
 /**
