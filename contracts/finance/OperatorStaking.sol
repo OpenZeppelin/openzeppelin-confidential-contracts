643edef7388fc68ac979c292f5baf3d249f01e92
Arr00 <13561405+arr00@users.noreply.github.com>
2025-09-26 16:10:43 +0400

    update operator staking

--- contracts/finance/OperatorStaking.sol
+++ contracts/finance/OperatorStaking.sol
@@ -2,23 +2,30 @@
 
 pragma solidity ^0.8.27;
 
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {ProtocolStaking} from "./ProtocolStaking.sol";
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-abstract contract OperatorStaking is ERC4626 {
+contract OperatorStaking is ERC4626 {
     ProtocolStaking private _protocolStaking;
 
-    constructor(IERC20 asset, ProtocolStaking protocolStaking) ERC4626(asset) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        IERC20 asset,
+        ProtocolStaking protocolStaking
+    ) ERC20(name, symbol) ERC4626(asset) {
         _protocolStaking = protocolStaking;
         asset.approve(address(protocolStaking), type(uint256).max);
     }
 
+    function restake() public virtual {
+        _protocolStaking.stake(IERC20(asset()).balanceOf(address(this)));
+    }
+
     function totalAssets() public view virtual override returns (uint256) {
-        return
-            super.totalAssets() +
-            _protocolStaking.tokensInCooldown(address(this)) +
-            _protocolStaking.balanceOf(address(this));
+        return super.totalAssets() + _protocolStaking.balanceOf(address(this));
     }
 
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
