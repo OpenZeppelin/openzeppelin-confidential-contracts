1c8be1a87c69ca89aee04ab1b1c50575126b7f88
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2025-10-17 18:28:44 +0200

    Add rewarder doc

--- contracts/finance/OperatorRewarder.sol
+++ contracts/finance/OperatorRewarder.sol
@@ -11,6 +11,9 @@ import {OperatorStaking} from "./OperatorStaking.sol";
 import {ProtocolStaking} from "./ProtocolStaking.sol";
 
 /**
+ * @title OperatorRewarder
+ * @custom:security-contact security@zama.ai
+ * @notice Distributes protocol staking rewards to operator stakers, with optional owner fee.
  * @dev A rewarder contract that works in tandem with `OperatorStaking` and `ProtocolStaking` contracts.
  * This contract receives rewards directly from `ProtocolStaking` and distributes them to `OperatorStaking` staker.
  * The owner of this contract can opt to take a fee on the rewards.
@@ -29,11 +32,19 @@ contract OperatorRewarder is Ownable {
     int256 private _totalVirtualRewardsPaid;
     mapping(address => int256) private _rewardsPaid;
 
+    /// @notice Emitted when the contract is shut down.
     event Shutdown();
+
+    /// @notice Emitted when the owner fee is updated.
     event OwnerFeeUpdated(uint16 oldFee, uint16 newFee);
 
+    /// @notice Error for unauthorized caller (not OperatorStaking).
     error CallerNotOperatorStaking(address caller);
+
+    /// @notice Error for attempting to shutdown when already shutdown.
     error AlreadyShutdown();
+
+    /// @notice Error for invalid basis points value.
     error InvalidBasisPoints(uint16 basisPoints);
 
     modifier onlyOperatorStaking() {
@@ -47,7 +58,10 @@ contract OperatorRewarder is Ownable {
         _operatorStaking = operatorStaking_;
     }
 
-    /// @dev Claims reward of a staker.
+    /**
+     * @notice Claims rewards for a staker.
+     * @param account The staker's address.
+     */
     function claimRewards(address account) public virtual {
         uint256 earned_ = earned(account);
         if (earned_ > 0) {
@@ -57,7 +71,9 @@ contract OperatorRewarder is Ownable {
         }
     }
 
-    /// @dev Claims owner fee.
+    /**
+     * @notice Claims owner fee.
+     */
     function claimOwnerFee() public virtual {
         uint256 totalAssetsPlusPaidRewards = _totalAssetsPlusPaidRewards();
         uint256 unpaidOwnerFee_ = _unpaidOwnerFee(totalAssetsPlusPaidRewards);
@@ -68,10 +84,8 @@ contract OperatorRewarder is Ownable {
     }
 
     /**
-     * @dev Sets the owner fee to `basisPoints`. Each basis point represents 1/100th of a percent
-     * of future earnings which will go to the owner.
-     *
-     * NOTE: `basisPoints` must be less than or equal to 10000.
+     * @notice Sets the owner fee in basis points (1/100th of a percent).
+     * @param basisPoints Fee in basis points (max 10000).
      */
     function setOwnerFee(uint16 basisPoints) public virtual onlyOwner {
         require(basisPoints <= 10000, InvalidBasisPoints(basisPoints));
@@ -82,7 +96,8 @@ contract OperatorRewarder is Ownable {
     }
 
     /**
-     * @dev Shutdown the rewarder contract. Practically, this means the contract no longer considers unclaimed
+     * @notice Shuts down the rewarder contract.
+     * @dev Practically, this means the contract no longer considers unclaimed
      * earnings from the `ProtocolStaking` contract as an asset.
      */
     function shutdown() public virtual onlyOperatorStaking {
@@ -92,7 +107,12 @@ contract OperatorRewarder is Ownable {
         emit Shutdown();
     }
 
-    /// @dev Handle required account on the transfer of `OperatorStaking` shares.
+    /**
+     * @notice Handles transfer of OperatorStaking shares, updating virtual rewards.
+     * @param from Sender address.
+     * @param to Recipient address.
+     * @param shares Number of shares transferred.
+     */
     function transferHook(address from, address to, uint256 shares) public virtual onlyOperatorStaking {
         uint256 oldTotalSupply = operatorStaking().totalSupply();
         if (oldTotalSupply == 0) return;
@@ -114,30 +134,51 @@ contract OperatorRewarder is Ownable {
         _totalVirtualRewardsPaid = totalVirtualRewardsPaid;
     }
 
-    /// @dev Gets staking token address.
+    /**
+     * @notice Returns the staking token address.
+     * @return The IERC20 staking token.
+     */
     function token() public view returns (IERC20) {
         return _token;
     }
 
+    /**
+     * @notice Returns the ProtocolStaking contract address.
+     * @return The ProtocolStaking contract.
+     */
     function protocolStaking() public view returns (ProtocolStaking) {
         return _protocolStaking;
     }
 
+    /**
+     * @notice Returns the OperatorStaking contract address.
+     * @return The OperatorStaking contract.
+     */
     function operatorStaking() public view returns (OperatorStaking) {
         return _operatorStaking;
     }
 
-    /// @dev Returns true if shutdown.
+    /**
+     * @notice Returns true if contract is shutdown.
+     * @return True if shutdown, false otherwise.
+     */
     function isShutdown() public view returns (bool) {
         return _shutdown;
     }
 
-    /// @dev Gets owner fee basis points.
+    /**
+     * @notice Returns the owner fee in basis points.
+     * @return Fee in basis points.
+     */
     function ownerFeeBasisPoints() public view returns (uint16) {
         return _ownerFeeBasisPoints;
     }
 
-    /// @dev Gets unpaid reward of a staker.
+    /**
+     * @notice Returns unpaid reward for a staker.
+     * @param account The staker's address.
+     * @return Amount of unpaid reward.
+     */
     function earned(address account) public view virtual returns (uint256) {
         uint256 stakedBalance = operatorStaking().balanceOf(account);
         int256 allocation = SafeCast.toInt256(
@@ -150,7 +191,10 @@ contract OperatorRewarder is Ownable {
         return SafeCast.toUint256(allocation - paid);
     }
 
-    /// @dev Gets unpaid reward of owner.
+    /**
+     * @notice Returns unpaid owner fee.
+     * @return Amount of unpaid owner fee.
+     */
     function unpaidOwnerFee() public view virtual returns (uint256) {
         return _unpaidOwnerFee(_totalAssetsPlusPaidRewards());
     }
