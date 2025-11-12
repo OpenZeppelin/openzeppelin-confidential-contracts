8412a6342db6929f6acb2885ff81d2c3e0a36c9f
Arr00 <13561405+arr00@users.noreply.github.com>
2025-11-12 11:54:39 -0300

    N-02: reorder allowances omnibus (#250)

--- contracts/token/ERC7984/extensions/ERC7984Omnibus.sol
+++ contracts/token/ERC7984/extensions/ERC7984Omnibus.sol
@@ -188,8 +188,6 @@ abstract contract ERC7984Omnibus is ERC7984 {
         euint64 amount,
         bytes calldata data
     ) internal virtual returns (euint64) {
-        euint64 transferred = confidentialTransferFromAndCall(omnibusFrom, omnibusTo, amount, data);
-
         FHE.allowThis(sender);
         FHE.allow(sender, omnibusFrom);
         FHE.allow(sender, omnibusTo);
@@ -197,6 +195,8 @@ abstract contract ERC7984Omnibus is ERC7984 {
         FHE.allowThis(recipient);
         FHE.allow(recipient, omnibusFrom);
         FHE.allow(recipient, omnibusTo);
+
+        euint64 transferred = confidentialTransferFromAndCall(omnibusFrom, omnibusTo, amount, data);
 
         FHE.allowThis(transferred);
         FHE.allow(transferred, omnibusFrom);
