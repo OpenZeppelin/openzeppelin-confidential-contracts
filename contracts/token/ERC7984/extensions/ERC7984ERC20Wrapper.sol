2cf214ceb948f77bbc7209c88662e825d26e4e49
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2025-12-22 10:41:05 +0100

    Inherit ERC7984ERC20Wrapper doc from interface

--- contracts/token/ERC7984/extensions/ERC7984ERC20Wrapper.sol
+++ contracts/token/ERC7984/extensions/ERC7984ERC20Wrapper.sol
@@ -49,11 +49,7 @@ abstract contract ERC7984ERC20Wrapper is ERC7984, IERC7984ERC20Wrapper {
         }
     }
 
-    /**
-     * @dev `ERC1363` callback function which wraps tokens to the address specified in `data` or
-     * the address `from` (if no address is specified in `data`). This function refunds any excess tokens
-     * sent beyond the nearest multiple of {rate} to `from`. See {wrap} from more details on wrapping tokens.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function onTransferReceived(
         address /*operator*/,
         address from,
@@ -75,11 +71,7 @@ abstract contract ERC7984ERC20Wrapper is ERC7984, IERC7984ERC20Wrapper {
         return IERC1363Receiver.onTransferReceived.selector;
     }
 
-    /**
-     * @dev Wraps amount `amount` of the underlying token into a confidential token and sends it to
-     * `to`. Tokens are exchanged at a fixed rate specified by {rate} such that `amount / rate()` confidential
-     * tokens are sent. Amount transferred in is rounded down to the nearest multiple of {rate}.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function wrap(address to, uint256 amount) public virtual {
         // take ownership of the tokens
         SafeERC20.safeTransferFrom(underlying(), msg.sender, address(this), amount - (amount % rate()));
@@ -88,22 +80,13 @@ abstract contract ERC7984ERC20Wrapper is ERC7984, IERC7984ERC20Wrapper {
         _mint(to, FHE.asEuint64(SafeCast.toUint64(amount / rate())));
     }
 
-    /**
-     * @dev Unwraps tokens from `from` and sends the underlying tokens to `to`. The caller must be `from`
-     * or be an approved operator for `from`. `amount * rate()` underlying tokens are sent to `to`.
-     *
-     * NOTE: The unwrap request created by this function must be finalized by calling {finalizeUnwrap}.
-     * NOTE: The caller *must* already be approved by ACL for the given `amount`.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function unwrap(address from, address to, euint64 amount) public virtual {
         require(FHE.isAllowed(amount, msg.sender), ERC7984UnauthorizedUseOfEncryptedAmount(amount, msg.sender));
         _unwrap(from, to, amount);
     }
 
-    /**
-     * @dev Variant of {unwrap} that passes an `inputProof` which approves the caller for the `encryptedAmount`
-     * in the ACL.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function unwrap(
         address from,
         address to,
@@ -113,7 +96,7 @@ abstract contract ERC7984ERC20Wrapper is ERC7984, IERC7984ERC20Wrapper {
         _unwrap(from, to, FHE.fromExternal(encryptedAmount, inputProof));
     }
 
-    /// @dev Fills an unwrap request for a given cipher-text `burntAmount` with the `cleartextAmount` and `decryptionProof`.
+    /// @inheritdoc IERC7984ERC20Wrapper
     function finalizeUnwrap(
         euint64 burntAmount,
         uint64 burntAmountCleartext,
@@ -140,37 +123,27 @@ abstract contract ERC7984ERC20Wrapper is ERC7984, IERC7984ERC20Wrapper {
         return _decimals;
     }
 
-    /**
-     * @dev Returns the rate at which the underlying token is converted to the wrapped token.
-     * For example, if the `rate` is 1000, then 1000 units of the underlying token equal 1 unit of the wrapped token.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function rate() public view virtual returns (uint256) {
         return _rate;
     }
 
-    /// @dev Returns the address of the underlying ERC-20 token that is being wrapped.
+    /// @inheritdoc IERC7984ERC20Wrapper
     function underlying() public view returns (IERC20) {
         return _underlying;
     }
 
-    /// inheritdoc IERC165
+    /// @inheritdoc IERC165
     function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC7984) returns (bool) {
         return interfaceId == type(IERC7984ERC20Wrapper).interfaceId || super.supportsInterface(interfaceId);
     }
 
-    /**
-     * @dev Returns the underlying balance divided by the {rate}, a value greater or equal to the actual
-     * {confidentialTotalSupply}.
-     *
-     * NOTE: The return value of this function can be inflated by directly sending underlying tokens to the wrapper contract.
-     * Reductions will lag compared to {confidentialTotalSupply} since it is updated on {unwrap} while this function updates
-     * on {finalizeUnwrap}.
-     */
+    /// @inheritdoc IERC7984ERC20Wrapper
     function totalSupply() public view virtual returns (uint256) {
         return underlying().balanceOf(address(this)) / rate();
     }
 
-    /// @dev Returns the maximum total supply of wrapped tokens supported by the encrypted datatype.
+    /// @inheritdoc IERC7984ERC20Wrapper
     function maxTotalSupply() public view virtual returns (uint256) {
         return type(uint64).max;
     }
