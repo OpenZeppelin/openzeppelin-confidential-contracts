4dde8eb2902defb0f61dfee995c0ac993cbdeebf
Arr00 <13561405+arr00@users.noreply.github.com>
2026-01-14 08:58:49 -0600

    up

--- contracts/utils/BatcherConfidential.sol
+++ contracts/utils/BatcherConfidential.sol
@@ -1,22 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-import {FHE, externalEuint64, euint64, ebool} from "@fhevm/solidity/lib/FHE.sol";
+import {FHE, externalEuint64, euint64, ebool, euint128} from "@fhevm/solidity/lib/FHE.sol";
 import {ERC7984ERC20Wrapper} from "../token/ERC7984/extensions/ERC7984ERC20Wrapper.sol";
 import {FHESafeMath} from "./../utils/FHESafeMath.sol";
 
 pragma solidity ^0.8.24;
 
-contract BatcherConfidential {
+abstract contract BatcherConfidential {
     ERC7984ERC20Wrapper private _fromToken;
     ERC7984ERC20Wrapper private _toToken;
     mapping(uint256 => Batch) private _batches;
     uint256 private _currentBatchId = 1;
 
     struct Batch {
-        euint64 confidentialAmount;
+        euint64 amountFromToken;
         euint64 unwrapAmount;
+        uint256 exchangeRate;
         mapping(address => euint64) deposits;
     }
+
+    error BatchNotFinalized();
 
     constructor(ERC7984ERC20Wrapper fromToken_, ERC7984ERC20Wrapper toToken_) {
         _fromToken = fromToken_;
@@ -32,18 +35,30 @@ contract BatcherConfidential {
         FHE.allowThis(newDeposits);
         _batches[_currentBatchId].deposits[msg.sender] = newDeposits;
 
-        euint64 newTotalDeposits = FHE.add(_batches[_currentBatchId].confidentialAmount, transferred);
+        euint64 newTotalDeposits = FHE.add(_batches[_currentBatchId].amountFromToken, transferred);
         FHE.allowThis(newTotalDeposits);
-        _batches[_currentBatchId].confidentialAmount = newTotalDeposits;
+        _batches[_currentBatchId].amountFromToken = newTotalDeposits;
     }
 
-    function exit() public {}
+    function exit(uint256 batchId) public {
+        require(_batches[batchId].exchangeRate != 0, BatchNotFinalized());
+
+        euint64 deposit = _batches[batchId].deposits[msg.sender];
+        _batches[batchId].deposits[msg.sender] = euint64.wrap(0);
+
+        euint128 amountToSend = FHE.div(
+            FHE.mul(FHE.asEuint128(deposit), uint128(_batches[batchId].exchangeRate)),
+            uint128(1e18)
+        );
+        toToken().confidentialTransfer(msg.sender, FHE.asEuint64(amountToSend));
+    }
 
     function dispatchBatch() public {
         uint256 batchId = _currentBatchId++;
-        euint64 amountToUnwrap = _batches[batchId].confidentialAmount;
-        FHE.allowTransient(amountToUnwrap, address(fromToken()));
+        euint64 amountToUnwrap = _batches[batchId].amountFromToken;
         _batches[batchId].unwrapAmount = _calculateUnwrapAmount(amountToUnwrap);
+
+        FHE.allowTransient(amountToUnwrap, address(fromToken()));
         fromToken().unwrap(address(this), address(this), amountToUnwrap);
     }
 
@@ -56,9 +71,20 @@ contract BatcherConfidential {
 
         // May revert since anyone can finalize on behalf of the contract
         try fromToken().finalizeUnwrap(unwrapAmount, unwrapAmountCleartext, decryptionProof) {} catch {}
+
+        bytes32[] memory handles = new bytes32[](1);
+        handles[0] = euint64.unwrap(unwrapAmount);
+
+        bytes memory cleartexts = abi.encode(unwrapAmountCleartext);
+
+        FHE.checkSignatures(handles, cleartexts, decryptionProof);
+
+        _executeRoute(unwrapAmountCleartext);
     }
 
-    function finalizeBatch() public {}
+    function finalizeBatch() public {
+        // _executeRoute();
+    }
 
     function fromToken() public view returns (ERC7984ERC20Wrapper) {
         return _fromToken;
@@ -71,6 +97,8 @@ contract BatcherConfidential {
     function batchUnwrapAmount(uint256 batchId) public view returns (euint64) {
         return _batches[batchId].unwrapAmount;
     }
+
+    function _executeRoute(uint256 amount) internal virtual;
 
     /// @dev Mirror calculations done on the token to attain the same cipher-text for unwrap tracking.
     function _calculateUnwrapAmount(euint64 requestedUnwrapAmount) private returns (euint64) {
