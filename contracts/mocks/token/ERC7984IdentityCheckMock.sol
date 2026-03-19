eb9f2fc0de122ddd66fc34af0bc6fc7197e3bb79
Arr00 <13561405+arr00@users.noreply.github.com>
2026-03-19 15:41:55 -0400

    remove unused imports

--- contracts/mocks/token/ERC7984IdentityCheckMock.sol
+++ contracts/mocks/token/ERC7984IdentityCheckMock.sol
@@ -2,11 +2,8 @@
 
 pragma solidity ^0.8.27;
 
-import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
-import {FHE, euint64, externalEuint64} from "@fhevm/solidity/lib/FHE.sol";
-import {ERC7984} from "../../token/ERC7984/ERC7984.sol";
+import {euint64} from "@fhevm/solidity/lib/FHE.sol";
 import {ERC7984IdentityCheck} from "../../token/ERC7984/extensions/ERC7984IdentityCheck.sol";
-import {HandleAccessManager} from "../../utils/HandleAccessManager.sol";
 import {ERC7984Mock} from "./ERC7984Mock.sol";
 
 contract ERC7984IdentityCheckMock is ERC7984Mock, ERC7984IdentityCheck {
