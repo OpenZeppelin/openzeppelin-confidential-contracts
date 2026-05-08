2b2b927d0f02161519ee138fe884d6da023da5d1
Arr00 <13561405+arr00@users.noreply.github.com>
2026-05-07 18:03:02 -0600

    add event doc

--- contracts/interfaces/IERC7984Rwa.sol
+++ contracts/interfaces/IERC7984Rwa.sol
@@ -7,6 +7,7 @@ import {IERC7984} from "./IERC7984.sol";
 
 /// @dev Interface for confidential RWA contracts.
 interface IERC7984Rwa is IERC7984 {
+    /// @dev Emitted when the balance `amount` of `lostAccount` is recovered to `newAccount`.
     event TokensRecovered(address indexed lostAccount, address indexed newAccount, euint64 amount);
 
     /// @dev Returns true if the contract is paused, false otherwise.
