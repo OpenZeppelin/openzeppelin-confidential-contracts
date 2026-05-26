ccb4572df1585bb69de4efeea0f134d2cd24e360
Arr00 <13561405+arr00@users.noreply.github.com>
2026-05-26 18:27:50 -0500

    Remove unused imports (#374)

--- contracts/finance/BatcherConfidential.sol
+++ contracts/finance/BatcherConfidential.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.27;
 
-import {FHE, externalEuint64, euint64, ebool, euint128} from "@fhevm/solidity/lib/FHE.sol";
+import {FHE, externalEuint64, euint64, ebool} from "@fhevm/solidity/lib/FHE.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
