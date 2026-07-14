40f256d03b45c557989ec79801aca8589c0d9f46
Arr00 <13561405+arr00@users.noreply.github.com>
2026-07-14 19:09:27 +0100

    fix test

--- test/token/ERC7984/extensions/ERC7984ERC20Wrapper.test.ts
+++ test/token/ERC7984/extensions/ERC7984ERC20Wrapper.test.ts
@@ -335,7 +335,12 @@ describe('ERC7984ERC20Wrapper', function () {
     it('returns unwrap amount', async function () {
       await this.wrapper
         .connect(this.holder)
-        .$_unwrap(this.holder, this.holder, await this.wrapper.confidentialBalanceOf(this.holder.address), 0);
+        .$_unwrap(
+          this.holder,
+          this.holder,
+          await this.wrapper.confidentialBalanceOf(this.holder.address),
+          `0x${'00'.repeat(12)}`,
+        );
 
       const [unwrapAmount] = (await this.wrapper.queryFilter(this.wrapper.filters.return$_unwrap()))[0].args;
       await expect(this.wrapper.unwrapRequester(unwrapAmount)).to.eventually.eq(this.holder);
