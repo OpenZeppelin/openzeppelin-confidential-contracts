3d9f98e89a56f2501f10443f8f7ad67c157406c6
Arr00 <13561405+arr00@users.noreply.github.com>
2026-07-27 15:02:17 -0400

    Enable foundry testing

---
+++ test/TempTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {FhevmTest} from "forge-fhevm/FhevmTest.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract TempTest is FhevmTest {
+    function setUp() public override {}
+}
